39439dd4a50be70d4f9843c461efe90ef3e6132d
Raul Ruiz <ruiz.carau25@triana.salesianos.edu>
2025-11-18 09:48:25 +0100

    Proyecto terminado

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -2,14 +2,19 @@ package modelo;
 public class Persona {
     private String nombre;
     private int edad;
+    private String curso;
 
-    public Persona(String nombre, int edad) {
+
+    public Persona(String nombre, int edad, String curso) {
         this.nombre = nombre;
         this.edad = edad;
+        this.curso = curso;
     }
+
 
     public void mostrarDatos() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
+        System.out.println("Curso: " + curso);
     }
 }
--- src/modelo/Programa.java
+++ src/modelo/Programa.java
@@ -1,7 +1,7 @@
 package modelo;
 public class Programa {
     public static void main(String[] args) {
-        Persona p = new Persona("Ana", 20);
+    	Persona p = new Persona("Ana", 20, "DAM 1");
         p.mostrarDatos();
     }
 }
